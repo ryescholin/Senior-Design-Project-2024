87290ede655822fe7135750391913835716b6055
Om <omarv321@gmail.com>
2024-11-16 13:52:50 -0800

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ def main():
     fault_detector = FaultDetection(grid)
 
     route_values_sequence = [
-        {"Route 1": 300, "Route 2": 300},    # Loop 1: No faults
-        {"Route 1": 300, "Route 2": 300},    # Loop 2: No faults
-        {"Route 1": 800, "Route 2": 300},    # Loop 3: Fault on 11-12, triggers rerouting
-        {"Route 1": 300, "Route 2": 300},    # Loop 4: No new faults
-        {"Route 1": 300, "Route 2": 800},    # Loop 5: Fault on 21-22, triggers rerouting
-        {"Route 1": 300, "Route 2": 300}     # Loop 6: No new faults
+        {"Route 1": 100},
+        {"Route 1": 200},   
+        {"Route 1": 300},    
+        {"Route 1": 300},  
+        {"Route 1": 300},   
+        {"Route 1": 300}     
     ]
 
     for count, route_values in enumerate(route_values_sequence):
@@ -23,7 +23,7 @@ def main():
         else:
             print("Faults detected on lines:", faults)
         
-        # Print detailed route status after fault detection and rerouting
+        # Print route status after fault detection and rerouting
         grid.verify_power_restoration()
         time.sleep(1)
 
