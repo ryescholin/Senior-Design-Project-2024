653769b960238aaeec5cfba0e350fd793f9b056c
sync7319 <omarv321@gmail.com>
2025-01-22 17:56:23 -0800

    letters added

--- senior_design_Jan_15_2025/initial_values.py
+++ senior_design_Jan_15_2025/initial_values.py
@@ -7,28 +7,28 @@ async def setup_initial_values():
     # Define addresses for Bluetooth devices
     pico1_address = "28:CD:C1:11:90:2E"
     pico2_address = "28:CD:C1:0E:C3:D7"
-
+    print('a')
     # Create BluetoothDevice objects for Pico 1 (closed) and Pico 2 (open)
     pico1 = BluetoothDevice(pico1_address, fault_upper_limit=250, closed=True)
     pico2_end = BluetoothDevice(pico2_address, fault_upper_limit=100, closed=False, connections={"Route 1": True, "powerline": True})
-
+    print('b')
     # Define a grid and routes
     grid = Grid()
-
+    print('c')
     # Create a route with Pico devices
     route1 = Route(
         "Route 1",
         breakers=[pico1, pico2_end],
         end_breaker=pico2_end
     )
-
+    print('d')
     grid.add_route(route1)
-
+    print('e')
     # Open connections for both devices
     lock = asyncio.Lock()
     await asyncio.gather(
         pico1.open_connection(lock),
         pico2_end.open_connection(lock)
     )
-
+    print('f')
     return grid, pico1, pico2_end
